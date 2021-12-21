1d2fb81988c57b7821d12086fad3f524952ac01f
Norbert Kosmal <norbert.kosmal@gmail.com>
2021-12-21 15:12:06 +0100

    REST framework for Django

---
+++ admin/products/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product, User
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):  # /api/products
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):  # /api/products/{id}
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):  # /api/products/{id}
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):  # /api/products/{id}
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
