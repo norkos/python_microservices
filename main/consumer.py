db85a6678edb52220a4896d5036fd19da42b4199
Norbert Kosmal <norbert.kosmal@gmail.com>
2021-12-22 16:55:30 +0100

    REST api for Flask

--- main/consumer.py
+++ main/consumer.py
@@ -21,15 +21,20 @@ def callback(ch, method, properties, body):
         product = Product(id=data['id'], title=data['title'], image=data['image'])
         db.session.add(product)
         db.session.commit()
+        print('Product created')
+
     elif properties.content_type == 'product_updated':
         product = Product.query.get(data['id'])
         product.title = data['title']
         product.image = data['image']
         db.session.commit()
+        print('Product updated')
+
     elif properties.content_type == 'product_deleted':
         product = Product.query.get(data)
         db.session.delete(product)
         db.session.commit()
+        print('Product deleted')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from dataclasses import dataclass
+
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
@@ -9,8 +11,11 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
-
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
@@ -24,9 +29,9 @@ class ProductUser(db.Model):
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'Hello'
+    return jsonify(Product.query.all())
 
 
 if __name__ == '__main__':
