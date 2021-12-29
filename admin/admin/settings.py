b70c519b05258ea18dfdcdfd24cccfe0576ff99b
Norbert Kosmal <norbert.kosmal@vonage.com>
2021-12-29 20:32:01 +0100

    Creating new product in front-end

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['host.docker.internal', '127.0.0.1']
+ALLOWED_HOSTS = ['host.docker.internal', '127.0.0.1', 'localhost']
 
 
 # Application definition
--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav";
-import Menu from "./components/Menu";
 import Products from "./admin/Products";
 import {
     BrowserRouter,
     Route
 } from "react-router-dom";
 import Main from "./main/Main";
+import ProductCreate from "./admin/ProductCreate";
 
 function App() {
   return (
     <div className="App">
-
-
               <BrowserRouter>
                   <Route path='/' exact component={Main}/>
-                  <Route path='/admin/products/' component={Products}/>
+                  <Route path='/admin/products/' exact component={Products}/>
+                  <Route path='/admin/products/create' exact component={ProductCreate}/>
               </BrowserRouter>
 
     </div>
